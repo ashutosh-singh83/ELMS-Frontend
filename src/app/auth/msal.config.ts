5417747bc25e3ddc3b38c33ba94c26cad6b739d3
Ashutosh Durvijay Singh <ashutosh_durvijaysingh@epam.com>
2025-07-31 14:20:58 +0530

    redirecturl

--- src/app/auth/msal.config.ts
+++ src/app/auth/msal.config.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '5d0a07aa-f7da-4442-963b-8dc7b9e48c1e',
     authority: 'https://login.microsoftonline.com/b41b72d0-4e9f-4c26-8a69-f949f367c91d',
-    redirectUri: 'http://localhost:4200'
+    redirectUri: 'https://kind-glacier-0b1f36800.2.azurestaticapps.net'
   },
   cache: {
     cacheLocation: 'localStorage',
